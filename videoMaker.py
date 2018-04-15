b9bc96b6aae0d6930f119a804fdeb56a0394f44a
dan <dan@localhost.localdomain>
2018-04-15 15:41:57 +0100

    initial commit

---
+++ videoMaker.py
@@ -0,0 +1,94 @@
+
+# Import all the things!
+import os
+import shutil
+import requests
+from time import sleep
+from datetime import date, timedelta
+from twython import Twython
+from auth import (
+    consumer_key,
+    consumer_secret,
+    access_token,
+    access_token_secret
+)
+
+
+# Function to check internet connectivity
+def checkConnection():
+    try:
+        requests.get("http://google.com", timeout=3)
+        return True
+    except requests.ConnectionError:
+        pass
+
+    return False
+
+
+# Function to tweet video
+def tweetVideo(fileName):
+    # Set attempts for while loop below
+    attempt = 0
+    maxAttempts = 5
+
+    while attempt < maxAttempts:
+        # This section posts the video to twitter if the internet is connected
+        if checkConnection():
+            twitter = Twython(
+                consumer_key, consumer_secret,
+                access_token, access_token_secret)
+
+            message = 'Garden timelapse - %s' % yesterday.strftime('%d-%m-%Y')
+            video = open(fileName, 'rb')
+            response = twitter.upload_video(
+                media=video,
+                media_type='video/mp4',
+                media_category='tweet_video',
+                check_progress=True)
+
+            twitter.update_status(
+                status=message, media_ids=[response['media_id']])
+            return
+
+        # If internet is not currently available then wait and retry
+        else:
+            sleep(15)
+            attempt += 1
+
+
+# Generate string for yesterdays files
+yesterday = date.today() - timedelta(1)
+yesterdayStr = yesterday.strftime('%Y-%m-%d')
+
+dirs = {
+    'archiveDir': 'archive',
+    'outputDir': 'videos',
+    'inputDir': 'Camera1/%s' % yesterdayStr
+}
+
+# If the directories specified above doesn't exist then make them
+for key in dirs:
+    if not(os.path.exists(dirs[key])):
+        os.mkdir(dirs[key])
+
+# Generate filenames and directories
+# Input
+inputFiles = os.path.join(dirs['inputDir'], '*.jpg')
+# Output
+outputFile = os.path.join(dirs['outputDir'], '%s.mp4' % yesterdayStr)
+# Archive
+archiveFile = os.path.join(dirs['archiveDir'], yesterdayStr)
+
+# Generate mp4 from the still images recorded the day before
+os.system(
+    'cat %s | ' % inputFiles +
+    'ffmpeg -framerate 6 -f image2pipe -vcodec mjpeg -i - ' +
+    '-vcodec libx264 -b:v 2048k -y %s' % outputFile
+)
+
+# Archive the images used for the video and delete original folder
+shutil.make_archive(archiveFile, 'zip', dirs['inputDir'])
+shutil.rmtree(dir['inputDir'])
+
+# Try and tweet the videos
+tweetVideo(outputFile)
